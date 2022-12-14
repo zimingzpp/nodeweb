466d158fc0d23b7c31ec0ce658aa9e2872c7f9dc
zimingzpp <zimingzpp@qq.com>
2022-12-14 18:04:44 +0800

    v3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'V2' });
+  res.render('index', { title: 'V3' });
 });
 
 module.exports = router;
